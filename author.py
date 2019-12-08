475d981e785c425fd4807ee81135dd5c2adda823
Sasha Smirnova <sashaslon1998@gmail.com>
2019-12-08 15:40:05 +0300

    добавлен поиск по авторам

--- author.py
+++ author.py
@@ -1,5 +1,5 @@
 from csvtools import DumpTable
-
+from notes import ProzhitoNotes
 
 class ProzhitoAuthors(DumpTable):
     def __init__(self, dumpwrapper):
@@ -9,12 +9,11 @@ class ProzhitoAuthors(DumpTable):
     def load(self, filename):
         super().load(filename)
         for i in self.table_iterator:
-            a = ProzhitoAuthor()
+            a = ProzhitoAuthor(self.dw)
             a.loadraw(i)
             self.authors_list.append(a)
 
     def get_by_id(self, ID):
-        print('trying to find author with ID', ID)
         for a in self.authors_list:
             if a.ID == ID: return a
 
@@ -26,8 +25,9 @@ class ProzhitoAuthors(DumpTable):
 
 
 class ProzhitoAuthor:
-    def __init__(self):
+    def __init__(self, dumpwrap):
         self.raw = None
+        self.dw = dumpwrap
         # todo sasha
 
     def loadraw(self, r):
@@ -37,6 +37,22 @@ class ProzhitoAuthor:
         self.last_name = r[2]
         self.second_name = r[3]
         # todo sasha
+
+    @property
+    def diaries(self):
+        authors_diary = filter(lambda diary: diary.author_ID == self.ID,
+                               self.dw.diaries.diaries_list)
+        return list(authors_diary)
+
+    @property
+    def notes(self):
+        diaries_id = list(map(lambda diary: diary.ID, self.diaries))
+        author_notes = filter(lambda note: note.diary_ID in diaries_id, self.dw.notes)
+        return ProzhitoNotes.new_from_list(self.dw, author_notes)
+
+    @property
+    def name(self):
+        return "{0} {1} {2}".format(self.first_name, self.second_name, self.last_name)
 
     def __repr__(self):
        return '#{0} {1} {2} {3}'.format(self.ID, self.first_name,
